52ccae4ab6953d929827b1187492e048073ef1ff
Kezzarz <skylar74@outlook.fr>
2025-07-11 16:26:51 +0200

    ajout de la page recette

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipeListComponent } from './pages/recipe-list/recipe-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/recettes', pathMatch: 'full' },
+  { path: 'recettes', component: RecipeListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
